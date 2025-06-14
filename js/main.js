006e6faebbdce38e6b9b7a07a0d36861f5211a74
ddiong2525 <choijw2005@naver.com>
2025-06-15 01:10:08 +0900

    loading

--- js/main.js
+++ js/main.js
@@ -2,32 +2,41 @@ document.addEventListener('DOMContentLoaded', () => {
     gsap.registerPlugin(ScrollTrigger);
     ScrollTrigger.normalizeScroll(true);
 
-/*     let lastScroll = 0;
-    const nav = document.querySelector('nav');
+    /*     let lastScroll = 0;
+        const nav = document.querySelector('nav');
+    
+        window.addEventListener('scroll', function () {
+            const curr = window.scrollY;
+            if (curr > lastScroll && curr > 10) {
+                // 아래로 스크롤: nav 숨김
+                nav.style.transform = 'translateY(-100%)';
+            } else {
+                // 위로 스크롤: nav 노출
+                nav.style.transform = 'translateY(0)';
+            }
+            lastScroll = curr;
+        }); */
 
-    window.addEventListener('scroll', function () {
-        const curr = window.scrollY;
-        if (curr > lastScroll && curr > 10) {
-            // 아래로 스크롤: nav 숨김
-            nav.style.transform = 'translateY(-100%)';
-        } else {
-            // 위로 스크롤: nav 노출
-            nav.style.transform = 'translateY(0)';
+    /*     const lenis = new Lenis({
+            duration: 1.2, // 부드러움 정도 (1~2 추천)
+            smooth: true,
+            direction: 'vertical',
+            gestureOrientation: 'vertical',
+            smoothTouch: false, // 모바일에서 기본 스크롤 유지
+        });
+    
+        function raf(time) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
         }
-        lastScroll = curr;
-    }); */
+        requestAnimationFrame(raf); */
 
-/*     const lenis = new Lenis({
-        duration: 1.2, // 부드러움 정도 (1~2 추천)
-        smooth: true,
-        direction: 'vertical',
-        gestureOrientation: 'vertical',
-        smoothTouch: false, // 모바일에서 기본 스크롤 유지
-    });
+    setTimeout(() => {
+        document.querySelector('.loading').classList.add('hidden');
+        location.href = '#';
+    }, 3000);
 
-    function raf(time) {
-        lenis.raf(time);
-        requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf); */
+    setTimeout(() => {
+        document.querySelector('#con0 video').play();
+    }, 7000);
 });
